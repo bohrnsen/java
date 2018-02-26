91c3a99b09621f89e89cd3dc4b6ed5e4aeb9c543
Kai Böhrnsen <kai.bohrnsen@gmail.com>
2018-02-26 21:57:37 +0100

    fix: Add MethodData

---
+++ src/main/java/com/jsoniter/MethodData.java
@@ -0,0 +1,26 @@
+package com.jsoniter;
+
+import java.util.Arrays;
+
+public class MethodData {
+    int numberOfBranches;
+    boolean[] branchReached;
+
+    MethodData(int numberOfBranches) {
+        this.numberOfBranches = numberOfBranches;
+        this.branchReached = new boolean[numberOfBranches];
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(branchReached);
+    }
+
+    public double getCoverage() {
+        double count = 0;
+        for (boolean bool : this.branchReached) {
+            count += (bool ? 1 : 0);
+        }
+        return count / numberOfBranches;
+    }
+}
