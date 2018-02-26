81cce2760f8cdbaff18d6adaef990278b32eb51f
Johannes Westlund <johannes.wirkkala.westlund@gmail.com>
2018-02-26 23:55:14 +0100

    feat: Add coverage for parse function

--- src/main/java/com/jsoniter/spi/OmitValue.java
+++ src/main/java/com/jsoniter/spi/OmitValue.java
@@ -1,6 +1,10 @@
 package com.jsoniter.spi;
 
+import com.jsoniter.MethodData;
+
 import java.lang.reflect.Type;
+
+import static com.jsoniter.GlobalData.dictionary;
 
 public interface OmitValue {
 
@@ -136,59 +140,81 @@ public interface OmitValue {
         }
 
         public static OmitValue parse(Type valueType, String defaultValueToOmit) {
+            MethodData methodData =  new MethodData(20);
+            dictionary.put("OmitValue - parse", methodData);
+            methodData.branchReached[0] = true;
             if ("void".equals(defaultValueToOmit)) {
+                methodData.branchReached[1] = true;
                 return null;
             } else if ("null".equals(defaultValueToOmit)) {
+                methodData.branchReached[2] = true;
                 return new OmitValue.Null();
             } else if (boolean.class.equals(valueType)) {
+                methodData.branchReached[3] = true;
                 Boolean defaultValue = Boolean.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + " == %s");
             } else if (Boolean.class.equals(valueType)) {
+                methodData.branchReached[4] = true;
                 Boolean defaultValue = Boolean.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + " == %s.booleanValue()");
             } else if (int.class.equals(valueType)) {
+                methodData.branchReached[5] = true;
                 Integer defaultValue = Integer.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + " == %s");
             } else if (Integer.class.equals(valueType)) {
+                methodData.branchReached[6] = true;
                 Integer defaultValue = Integer.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + " == %s.intValue()");
             } else if (byte.class.equals(valueType)) {
+                methodData.branchReached[7] = true;
                 Byte defaultValue = Byte.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + " == %s");
             } else if (Byte.class.equals(valueType)) {
+                methodData.branchReached[8] = true;
                 Byte defaultValue = Byte.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + " == %s.byteValue()");
             } else if (short.class.equals(valueType)) {
+                methodData.branchReached[9] = true;
                 Short defaultValue = Short.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + " == %s");
             } else if (Short.class.equals(valueType)) {
+                methodData.branchReached[10] = true;
                 Short defaultValue = Short.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + " == %s.shortValue()");
             } else if (long.class.equals(valueType)) {
+                methodData.branchReached[11] = true;
                 Long defaultValue = Long.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + "L == %s");
             } else if (Long.class.equals(valueType)) {
+                methodData.branchReached[12] = true;
                 Long defaultValue = Long.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + "L == %s.longValue()");
             } else if (float.class.equals(valueType)) {
+                methodData.branchReached[13] = true;
                 Float defaultValue = Float.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + "F == %s");
             } else if (Float.class.equals(valueType)) {
+                methodData.branchReached[14] = true;
                 Float defaultValue = Float.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + "F == %s.floatValue()");
             } else if (double.class.equals(valueType)) {
+                methodData.branchReached[15] = true;
                 Double defaultValue = Double.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + "D == %s");
             } else if (Double.class.equals(valueType)) {
+                methodData.branchReached[16] = true;
                 Double defaultValue = Double.valueOf(defaultValueToOmit);
                 return new OmitValue.Parsed(defaultValue, defaultValueToOmit + "D == %s.doubleValue()");
             } else if (char.class.equals(valueType) && defaultValueToOmit.length() == 1) {
+                methodData.branchReached[17] = true;
                 Character defaultValue = defaultValueToOmit.charAt(0);
                 return new OmitValue.Parsed(defaultValue, "'" + defaultValueToOmit + "' == %s");
             } else if (Character.class.equals(valueType) && defaultValueToOmit.length() == 1) {
+                methodData.branchReached[18] = true;
                 Character defaultValue = defaultValueToOmit.charAt(0);
                 return new OmitValue.Parsed(defaultValue, "'" + defaultValueToOmit + "' == %s.charValue()");
             } else {
+                methodData.branchReached[19] = true;
                 throw new UnsupportedOperationException("failed to parse defaultValueToOmit: " + defaultValueToOmit);
             }
         }
