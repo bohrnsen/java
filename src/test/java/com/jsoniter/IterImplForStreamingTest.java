a3be912e72b3ed24ef5967233ef4a1cd2ed5c6d6
Kai Böhrnsen <kai.bohrnsen@gmail.com>
2018-02-28 02:34:39 +0100

    Added first test, not yet working

--- src/test/java/com/jsoniter/IterImplForStreamingTest.java
+++ src/test/java/com/jsoniter/IterImplForStreamingTest.java
@@ -1,6 +1,11 @@
 package com.jsoniter;
 
+import com.jsoniter.spi.JsonException;
 import junit.framework.TestCase;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+
+import java.io.ByteArrayInputStream;
 
 public class IterImplForStreamingTest extends TestCase {
 
@@ -9,5 +14,21 @@ public class IterImplForStreamingTest extends TestCase {
 		JsonIterator iter = JsonIterator.parse("1.7976931348623157e+308");
 		String number = IterImplForStreaming.readNumber(iter);
 		assertEquals(maxDouble, number);
+	}
+
+	public void testReadBackspace() throws Exception {
+
+
+
+
+		byte[] bytes = {(byte) '\\', (byte) 'u', (byte) 'D', (byte) '8', (byte) '0', (byte) '0',
+				(byte) '\\', (byte) 'u', (byte) 'D', (byte) '8', (byte) '0', (byte) '0',};
+		JsonIterator iter = JsonIterator.parse(new ByteArrayInputStream(bytes), 12);
+		try {
+			IterImplForStreaming.readStringSlowPath(iter,0);
+			fail();
+		} catch (JsonException e) {
+			assertEquals("invalid surrogate", e.getMessage());
+		}
 	}
 }
